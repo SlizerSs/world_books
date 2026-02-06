6c3e21976f4f459f67bc8db1442927beeda6e50f
SlizerSs <zerelaw@bk.ru>
2026-02-06 12:56:15 +0300

    made several pages

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from catalog import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^books/(?P<pk>\d+)$',
+            views.BookDetailView.as_view(),
+            name='book-detail'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 ]
